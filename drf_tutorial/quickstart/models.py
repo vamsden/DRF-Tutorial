adaaa20814544a95d21f52ff4c55015563528bef
vanstinger <vandykfernandes@hotmail.com>
2016-06-04 01:09:33 +0400

    Created Models User and Group

--- drf_tutorial/quickstart/models.py
+++ drf_tutorial/quickstart/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    url = models.URLField()
+    username = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    groups = models.ForeignKey('Group', on_delete=models.CASCADE)
+
+
+class Group(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=30)
